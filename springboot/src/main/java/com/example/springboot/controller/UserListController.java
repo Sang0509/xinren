03051780f7deabcc1d87fe4980cfc7c750e9f261
Sang0509 <sy980509@gmail.com>
2023-01-24 14:11:19 +0900

    初めてのコミット

--- springboot/src/main/java/com/example/springboot/controller/UserListController.java
+++ springboot/src/main/java/com/example/springboot/controller/UserListController.java
@@ -4,11 +4,9 @@ import com.example.springboot.dao.ListDao;
 import com.example.springboot.entity.User;
 import com.example.springboot.entity.UserList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.namespace.QName;
 import java.util.List;
 
 @RestController
@@ -21,4 +19,24 @@ public class UserListController {
     public List<UserList> findAll() {
         return listDao.findAll();
     }
+
+    @PostMapping
+    public void save(@RequestBody UserList userList) {
+        String name = listDao.findName(userList.getName());
+        if (name.equals(userList.getName())){
+            listDao.update(userList);
+        }else{
+            listDao.insert(userList);
+        }
+    }
+
+    @DeleteMapping("/{name}")
+    public boolean delete(@PathVariable Integer name) {
+        System.out.println(name);
+        if (name == null || name == 0) {
+            throw new RuntimeException("参数错误");
+        }
+        return listDao.deletByName(name) == 1;
+    }
 }
+
--- springboot/src/main/java/com/example/springboot/dao/ListDao.java
+++ springboot/src/main/java/com/example/springboot/dao/ListDao.java
@@ -1,7 +1,9 @@
 package com.example.springboot.dao;
 
+import com.example.springboot.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import com.example.springboot.entity.UserList;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -10,4 +12,13 @@ import java.util.List;
 public interface ListDao {
 
     List<UserList> findAll();
+
+    @Select("select ifnull((select name from `list` where `name`= #{name }), 0) as `name`;")
+    String findName(String name);
+
+    int insert(UserList userList);
+
+    int update(UserList userList);
+
+    int deletByName(Integer name);
 }
